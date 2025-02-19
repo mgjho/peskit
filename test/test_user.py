ec77ebf1b8a7d36f378a27f55f4f45b275c8a3c4
mgjho <mgjho96@gmail.com>
2025-02-20 00:11:29 +0900

    chore: add xrfit dependency and test

---
+++ test/test_user.py
@@ -0,0 +1,20 @@
+from peskit.sim import get_edc, get_cut
+import xrfit
+from lmfit.models import LorentzianModel
+from lmfit import CompositeModel, Model
+from peskit.fit.fermi_dirac.model import FermiDiracModel
+from peskit.fit.gaussian.model import GaussianConvolveModel
+from peskit.common.function import convolve
+# from lmfit.lineshapes import gaussian
+def test_user():
+    data = get_cut()
+    data.plot()    
+    model = LorentzianModel(prefix="p0_") * FermiDiracModel()
+    # model = CompositeModel(model, GaussianConvolveModel(sigma=1.0), convolve)
+    guess = data.fit.guess(model=model, input_core_dims="eV")
+
+    # g_center = guess.params.get(params_name="g_center")
+    # print(g_center)
+    # guess.params.assign()
+    # guess["g_center"].set(value=0.0, vary=False)
+    result = data.fit(model, guess, input_core_dims="eV", method="least_squares")
